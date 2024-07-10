bcddfccc019b7bdcc7665038299759af9c1804f1
Darrell Jenkins <142088111+darrellajenkins@users.noreply.github.com>
2024-07-09 21:13:15 -0700

    Update product.py

--- product.py
+++ product.py
@@ -1,4 +1,5 @@
 from itertools import product as prod
+from itertools import product
 
 
 animals = ['camels', 'dogs', 'pigeons']
@@ -10,3 +11,12 @@ print()
 
 animales = ['camels', 'dogs', 'pigeons']
 print(list(prod(animales, repeat=2)))
+
+hours = ["12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+mins = ['00', '15', '30', '45']
+times = list(product(hours, mins))
+
+for part in ['AM', 'PM']:
+    for a, b in times:
+        print(f"{a}:{b}{part}")
+      
