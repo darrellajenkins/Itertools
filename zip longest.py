a79e702a65c61dbf1267f9dbbfc95ace8d44f904
Darrell <darrellajenkins@gmail.com>
2024-07-13 00:00:15 -0700

    Added background/origin zip function.

--- zip longest.py
+++ zip longest.py
@@ -13,3 +13,11 @@ workers = ['Alice', 'Bob', 'Charlie']
 
 for task, worker in zip_longest(tasks, workers, fillvalue=workers[-1]):
     print(f"{worker} does task {task}")
+
+# Homemade zip function that can be customized for many different uses.
+a = [1, 2, 3, 4]
+b = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+c = []
+for r in range(min(len(a), len(b))):
+    c.append((a[r], b[r]))
+print(c)
